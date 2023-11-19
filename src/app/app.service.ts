5f596cea759f652bc6d8f3df7f764e1185427d33
KrlKor <litcat@mail.ru>
2023-11-19 17:48:45 +0300

    add black burgers

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,6 +12,6 @@ export class AppService {
     return this.http.post('https://testologia.site/burgers-order', data)
   }
   detData() {
-    return this.http.get('https://testologia.site/burgers-data')
+    return this.http.get('https://testologia.site/burgers-data?extra=black')
   }
 }
