1d1cdd2a4a60a814307588d3984b7664f0682d0a
KrlKor <litcat@mail.ru>
2023-11-19 17:00:48 +0300

    create send & get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -17,106 +18,13 @@ export class AppComponent {
     phone: ['', Validators.required],
   })
 
-  productsData = [
-    {
-      image: "Burger1.png",
-      title: "Бургер чеддер & бекон",
-      text: "Котлета из говядины криспи, булочка, томат, сыр Чеддер, грудинка, лук красный, салат айсбер,майонез, кетчуп, сырный соус",
-      price: 8,
-      basePrice: 8,
-      grams: 360
-    },
-    {
-      image: "Burger2.png",
-      title: "BBQ с беконом и курицей",
-      text: "Булочка бриошь с кунжутом, куриная котлета, сыр чеддер, томат, огурец маринованный, лук маринованный, салат Ромен, бекон, соус BBQ",
-      price: 7,
-      basePrice: 7,
-      grams: 390
-    },
-    {
-      image: "Burger3.png",
-      title: "Дабл биф бургер",
-      text: "Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы, свежий томат, бекон, красный лук, соус бургер, горчица",
-      price: 10,
-      basePrice: 10,
-      grams: 420
-    },
-    {
-      image: "Burger4.png",
-      title: "Баварский бургер",
-      text: "Булочка для бургера, говяжья котлета, красный лук, сыр, охотничья колбаска, соус барбекю, соус сырный, салат айсберг",
-      price: 7,
-      basePrice: 7,
-      grams: 220
-    },
-    {
-      image: "Burger5.png",
-      title: "Бекон чизбургер",
-      text: "Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, сыр, сырный соус, кетчуп, зелень",
-      price: 8,
-      basePrice: 8,
-      grams: 220
-    },
-    {
-      image: "Burger6.png",
-      title: "Индиана бургер",
-      text: "Булочка для бургера, котлета куриная, грудинка, яйцо, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень",
-      price: 9,
-      basePrice: 9,
-      grams: 320
-    },
-    {
-      image: "Burger7.png",
-      title: "Вегги бургер",
-      text: "Булочка для бургера, вегетарианская котлета, красный лук, сыр, свежий томат, соус барбекю, соус сырный, салат айсберг",
-      price: 8,
-      basePrice: 8,
-      grams: 280
-    },
-    {
-      image: "Burger8.png",
-      title: "Плаксивый Джо",
-      text: "Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень",
-      price: 7,
-      basePrice: 7,
-      grams: 380
-    },
-    {
-      image: "Burger9.png",
-      title: "Двойной чиз бургер",
-      text: "Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень",
-      price: 11,
-      basePrice: 11,
-      grams: 400
-    },
-    {
-      image: "Burger10.png",
-      title: "Двойной чиз бургер",
-      text: "Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо, салями, соус барбекю, соус сырный, салат айсберг, свежий томат",
-      price: 9,
-      basePrice: 9,
-      grams: 300
-    },
-    {
-      image: "Burger11.png",
-      title: "Цуккини бургер",
-      text: "Булочка для бургера, вегетарианская котлета из нута, цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень",
-      price: 8,
-      basePrice: 8,
-      grams: 320
-    },
-    {
-      image: "Burger12.png",
-      title: "Двойной бургер чеддар",
-      text: "Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный, томат, кетчуп, двойной сыр чеддар, горчица, зелень",
-      price: 9,
-      basePrice: 9,
-      grams: 360
-    }
-  ];
+  productsData: any;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.appService.detData().subscribe(data => this.productsData = data);
   }
 
   scrollTo(target: HTMLElement, burger?: any) {
@@ -128,8 +36,20 @@ export class AppComponent {
 
   confirmOrder() {
     if (this.form.valid) {
-      alert("Спасибо за заказ.\nМы скоро свяжемся с вами!");
-      this.form.reset();
+
+      this.appService.sendOrder(this.form.value)
+        .subscribe(
+          {
+            // успех
+            next: (responce: any) => {
+              alert(responce.message);
+              this.form.reset();
+            },
+            error: (responce) => {
+              alert(responce.error.message)
+            }
+          }
+        );
     }
   }
 
